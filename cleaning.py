4a51c468731b2198d4486745f9ba0fcb399dd9ed
Juckert <130921125+Juckert@users.noreply.github.com>
2025-06-14 19:32:18 +0500

    Update cleaning.py

--- cleaning.py
+++ cleaning.py
@@ -1,14 +1,19 @@
 import pandas as pd
 from .base import DataProcessing
+from .io.loader import decorator
 
 class CleanData(DataProcessing):
+    @decorator
     def run(self) -> pd.DataFrame:
+        """Remove duplicates and reset index"""
         self.result = self.data.drop_duplicates().reset_index(drop=True)
         return self.result
 
+    @decorator
     def info(self) -> str:
-        return "Удаление дубликатов и сброс индексов"
+        return "Removing duplicates and resetting index"
 
+    @decorator
     def get_answ(self) -> pd.DataFrame:
         if self.result is None:
             self.run()
